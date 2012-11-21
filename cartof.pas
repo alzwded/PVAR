2c6931106332ac38402f7927870b83afd03c86e8
Vlad Meșco <alzwded@gmail.com>
2012-11-21 12:22:01 +0200

    work on cartof, provide

--- cartof.pas
+++ cartof.pas
@@ -10,6 +10,9 @@ uses
 type
   TCartof = class(TPart)
     procedure InitMesh; override;
+    function GetBoundingBox: PBoundingBox; override;
+  private
+    m_bbox: TBoundingBox;
   end;
 
 implementation
@@ -66,6 +69,18 @@ begin
         Point3DFromCoords(c.x + 50, c.y + 100, c.z + 50),
         Point3DFromCoords(c.x - 50, c.y + 100, c.z + 50));
   Geometry.Add(e);
+end;
+
+function TCartof.GetBoundingBox: PBoundingBox;
+var
+  c: TPoint3D;
+begin
+  c := GetRotatedPoint(m_c);
+
+  m_bbox.p1 := Point3DFromCoords(c.x - 50, c.y, c.z - 50);
+  m_bbox.p2 := Point3DFromCoords(c.x + 50, c.y + 100, c.z + 50);
+
+  GetBoundingBox := @m_bbox;
 end;
 
 end.
--- provider.pas
+++ provider.pas
@@ -7,16 +7,38 @@ interface
 uses
   Classes, SysUtils, CoreUtils, GfxUtils;
 
+const
+  PROVIDE_ON_FRAME = 10;
+  OFF_SCREEN_POINT = -1e+27;
+
 type
   TProvider = class(AGrabber)
+    (* helper function to add entities outside of any grabing action *)
+    procedure AddStock(e: IWorldEntity);
+
     procedure Loop; override;
+    procedure Render(engine: PJakRandrEngine); override;
+  private
+    m_pc: integer;
   end;
 
 implementation
 
+procedure TProvider.Render(engine: PJakRandrEngine); begin end;
+
 procedure TProvider.Loop;
 begin
+  m_pc := (m_pc + 1) mod PROVIDE_ON_FRAME;
 
+  if m_pc = 0 then begin
+    Entities[0].MoveTo(GetRotatedPoint(Centre));
+  end;
+end;
+
+procedure TProvider.AddStock(e: IWorldEntity);
+begin
+  e.MoveTo(Point3DFromCoords(0, 0, OFF_SCREEN_POINT));
+  Entities.Add(e);
 end;
 
 end.
